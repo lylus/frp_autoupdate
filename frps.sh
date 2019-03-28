ca12ae3ff58c0f4191e12701b714bdca16597ede
lylus <lylus@qq.com>
2019-03-28 21:03:28 +0800

    Update frps.sh

--- frps.sh
+++ frps.sh
@@ -1,31 +1,73 @@
-#!/bin/bash  
-  
-rm -rf /usr/local/frps/temp
+#!/bin/bash
 
-mkdir /usr/local/frps/temp
+# 获取frps最新版本号
 
-cd /usr/local/frps/temp
+	api_url="https://api.github.com/repos/fatedier/frp/releases/latest"
 
-/usr/bin/wget https://lylus.cn/frps.app
+	new_ver=`curl ${PROXY} -s ${api_url} --connect-timeout 10| grep 'tag_name' | cut -d\" -f4`
 
-cp -f /usr/local/frps/temp/frps.app /usr/local/frps/frps.app
+	touch ./version.txt
+	cat <<EOF > ./version.txt
+${new_ver}
+EOF
 
-echo 'ud..ing...'
+	sed -i 's/v//g' ./version.txt
+	get_releases=$(cat ./version.txt)
 
-echo 'del old'
+	releases_url=https://github.com/fatedier/frp/releases/download/${new_ver}/frp_${get_releases}_linux_amd64.tar.gz
+	rm -rf ./version.txt
 
-rm -f /usr/local/frps/frps
 
-echo 'copy...'
+# 检测frps版本
 
-cp -f /usr/local/frps/frps.app /usr/local/frps/frps
 
-echo 'copy yes...'
+        old_ver=`/usr/local/frps/frps -v`
 
-cd /usr/local/frps/
+#       echo "check old frp version results are: $old_ver"
+        echo "检测到本机运行的版本: $old_ver"
 
-chmod 755 frps
+#       echo "check new frp version results are: $get_releases"
+        echo "检测到最新的版本: $get_releases"
 
-echo 'okok!yesyes!!!'
+if [ "$old_ver" = "$get_releases" ]
 
-/usr/local/frps/frps -c /usr/local/frps/frps.ini
+then
+
+echo "没有检测到新的版本!"
+#echo "check new frp version"
+
+else
+# 安装frps
+
+        rm -rf /usr/local/frps/temp
+
+        mkdir /usr/local/frps/temp
+
+        cd /usr/local/frps/temp
+
+#	wget --no-check-certificate ${releases_url}
+
+	wget -N --no-check-certificate ${releases_url}
+
+	tar -zxvf frp*.tar.gz
+
+	mv /usr/local/frps/temp/frp*/frps /usr/local/frps/frpsbak
+
+	rm -rf /usr/local/frps/temp/frp*
+	
+	cp -f /usr/local/frps/frpsbak /usr/local/frps/frps
+	
+        cd /usr/local/frps
+
+	chmod 755 frps
+
+     echo "检测到新版本，已更新完成!"
+ #   echo "no no no run frp"
+
+
+#运行frps
+
+
+fi
+
+#/usr/local/frps/frps -c /usr/local/frps/frps.ini
